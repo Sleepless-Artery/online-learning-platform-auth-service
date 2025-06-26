b475cc7efb6a38a12b8725fbade95f7fdec90b3e
ART3RY-G1N <cplusplus3414@gmail.com>
2025-06-26 08:19:14 +0300

    Updated Dockerfile and fixed database migration

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,18 +1,18 @@
 CREATE TABLE roles (
-  id bigserial PRIMARY KEY,
+  id BIGSERIAL PRIMARY KEY,
   role_name VARCHAR(255) NOT NULL UNIQUE
 );
 
 INSERT INTO roles (role_name) VALUES ('USER'), ('ADMIN'), ('STUDENT'), ('AUTHOR');
 
 CREATE TABLE credentials(
-  id bigserial PRIMARY KEY,
+  id BIGSERIAL PRIMARY KEY,
   email_address VARCHAR(255) NOT NULL UNIQUE,
   password_hash VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE credentials_roles(
-   id bigserial PRIMARY KEY,
+   id BIGSERIAL PRIMARY KEY,
    credential_id BIGINT NOT NULL,
    role_id BIGINT NOT NULL,
    FOREIGN KEY (credential_id) REFERENCES credentials(id),
